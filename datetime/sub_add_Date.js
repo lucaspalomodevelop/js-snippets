8d637a50d392ea6dc3f775709c059bcfa0b27ddc
lucaspalomodevelop <lucas.palomo@t-online.de>
2022-02-02 21:03:28 +0100

    little fix

--- datetime/sub_add_Date.js
+++ datetime/sub_add_Date.js
@@ -1,11 +1,11 @@
-let date_Add = (date = new Date(), { year = 0, month = 0, day = 0 }) => {
+let date_Add = ({ date = new Date(), year = 0, month = 0, day = 0 }) => {
   date.setFullYear(date.getFullYear() + year);
   date.setMonth(date.getMonth() + month);
   date.setDate(date.getDate() + day);
   return date;
 };
 
-let date_Sub = (date = new Date(), { year = 0, month = 0, day = 0 }) => {
+let date_Sub = ({ date = new Date(), year = 0, month = 0, day = 0 }) => {
   date.setFullYear(date.getFullYear() - year);
   date.setMonth(date.getMonth() - month);
   date.setDate(date.getDate() - day);
